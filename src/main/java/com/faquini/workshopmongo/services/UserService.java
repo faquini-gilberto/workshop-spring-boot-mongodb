5844b5ad7462aef2997443c8585f9ae4c0f61eb0
faquini-gilberto <faquini.gilberto@gmail.com>
2020-08-06 13:43:36 -0300

    Obtendo um usuario por Id

--- src/main/java/com/faquini/workshopmongo/services/UserService.java
+++ src/main/java/com/faquini/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.faquini.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.faquini.workshopmongo.domain.User;
 import com.faquini.workshopmongo.repositoty.UserRepository;
+import com.faquini.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,8 +17,12 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll() {
-	
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
