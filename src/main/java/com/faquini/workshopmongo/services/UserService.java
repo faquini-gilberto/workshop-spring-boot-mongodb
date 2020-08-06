abbd36274b78a65ccbae0989d28f916ab700740f
faquini-gilberto <faquini.gilberto@gmail.com>
2020-08-06 14:56:06 -0300

    Insercao de usuario com POST

--- src/main/java/com/faquini/workshopmongo/services/UserService.java
+++ src/main/java/com/faquini/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.faquini.workshopmongo.domain.User;
+import com.faquini.workshopmongo.dto.UserDto;
 import com.faquini.workshopmongo.repositoty.UserRepository;
 import com.faquini.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
